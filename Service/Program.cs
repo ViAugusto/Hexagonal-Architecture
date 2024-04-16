03af9fb71923c1a90e50bc35c26cf833e26ec05b
Vini <vinicius.augusto0704@gmail.com>
2024-04-16 00:04:46 -0300

    Added program main and worker

---
+++ Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services;
+using AWS;
+using Domain.Ports;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        host.Run();
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<Worker>();
+
+                services.AddSingleton<S3ApplicationService>();
+                services.AddSingleton<IS3Service, S3Adapter>();
+                services.AddSingleton<ISqsService, SqsAdapter>();
+            });
+
+}
---
+++ Service/Worker.cs
@@ -0,0 +1,35 @@
+﻿using Application.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Service
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly S3ApplicationService _applicationService;
+
+        public Worker(ILogger<Worker> logger, S3ApplicationService applicationService)
+        {
+            _logger = logger;
+            _applicationService = applicationService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Exeutado as: {DateTime.Now}");
+
+
+            string queue = "queue_do_vini";
+            try
+            {
+                await _applicationService.ProcessarMensagemSqsAsync(queue);
+                _logger.LogInformation($"{queue} executado");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro no processamento: {ex}");
+            }
+        }
+    }
+}
